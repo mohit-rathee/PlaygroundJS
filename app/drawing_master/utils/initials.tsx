d5f7d13773770fab18e952f23f1abe425b894f19
mohit <mohit.rathee2505@gmail.com>
2024-11-28 23:33:50 +0530

    better

--- app/drawing_master/utils/initials.tsx
+++ app/drawing_master/utils/initials.tsx
@@ -2,18 +2,9 @@ export const LAYER_THRESHOLD = 100
 
 export const IS_DEBUG_MODE = false
 
-let color_map_gap = 1;
+export const COLOR_MAP_GAP = IS_DEBUG_MODE?100000:1
 
-let is_draw_dots = false
-
-if(IS_DEBUG_MODE){
-     color_map_gap = 100000
-     is_draw_dots = true
-
-}
-export const COLOR_MAP_GAP = color_map_gap
-
-export const IS_DRAW_DOTS = is_draw_dots
+export const IS_DRAW_DOTS = IS_DEBUG_MODE?true:false
 
 export const RDP_NORMAL = 2;
 
@@ -24,9 +15,9 @@ export const MIN_DISTANCE_BTW_PTS= 10;
 
 export const CATMULL_ROM_PRECISION = 0.01
 
-export const REFINE_THRESHOLD = 5
+export const REFINE_THRESHOLD = 10
 
-export const DELTA_TIME = 10
+export const DELTA_TIME = 20
 
 export const initialStrokePointer: StrokePointer = {
     layer: 1,
