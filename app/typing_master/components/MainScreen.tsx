30287e8f5e35694c48f3e0e7366b9f72d636ac71
mohit <mohit.rathee2505@gmail.com>
2025-01-13 01:14:45 +0530

    updated MainScreen

--- app/typing_master/components/MainScreen.tsx
+++ app/typing_master/components/MainScreen.tsx
@@ -2,16 +2,24 @@ import { BlurryScreen, Scrollable } from "./Screens";
 import TypingArena from "./typingComponents/TypingArena";
 import ArenaProvider from "../context/ArenaContext";
 import Cursor from "./typingComponents/Cursor";
+import { Result } from "./Result";
+import { useContext } from "react";
+import { PageContext } from "../context/PageContext";
 
 export default function MainPallet() {
-    return (
-        <ArenaProvider>
-            <BlurryScreen>
-                <Scrollable>
-                    <TypingArena />
-                    <Cursor />
-                </Scrollable>
-            </BlurryScreen>
-        </ArenaProvider>
-    )
+    const { gameInfo } = useContext(PageContext)
+    if (gameInfo.type == 'result')
+        return <Result />
+    else
+        return (
+            <ArenaProvider>
+                <BlurryScreen>
+                    <Scrollable>
+                        <TypingArena />
+                        <Cursor />
+                    </Scrollable>
+                    <Result />
+                </BlurryScreen>
+            </ArenaProvider>
+        )
 }
