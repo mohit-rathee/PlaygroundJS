3e68710ab7d52ab20fced9ab7c9cab9a5d5fa264
mohit <mohit.rathee2505@gmail.com>
2025-01-20 15:20:45 +0530

    Calculate result

---
+++ app/typing_master/utils/result_utils.tsx
@@ -0,0 +1,112 @@
+import { GameInfoType, Timestamps } from "../types"
+
+const WPM = 60 / 5
+const ns = 100
+const items_in_1_sec = 1000 / ns
+const Lowest = 1
+const Low = 10
+const Mid = 30
+
+export function calculateResult(gameInfo: GameInfoType) {
+    if (gameInfo.type != 'result') throw Error('idk')
+    // console.log(gameInfo.timestamps)
+    const total_time = gameInfo.timestamps.length / items_in_1_sec
+    const steps = 25
+    let dots;
+    if (total_time < Lowest)
+        dots = items_in_1_sec / 1000 // 0.01 sec
+    else if (total_time < Low)
+        dots = items_in_1_sec / 2 // 0.5 sec
+    else if (total_time < Mid)
+        dots = items_in_1_sec
+    else
+        dots = Math.floor(gameInfo.timestamps.length / steps)
+    const aggregatedData = aggregateResut(dots, gameInfo.timestamps)
+    console.log('aggregatedData', aggregatedData)
+
+    const graphData = aggregatedData.map((v: any, idx: number) => {
+
+        const prev = aggregatedData[idx - 1] ||
+            { correct: 0, incorrect: 0, gross: 0, timestamps: 0 }
+
+        const correctTyped = v.correct - prev.correct;
+        const incorrectTyped = v.incorrect - prev.incorrect;
+        const gap = v.timestamps - prev.timestamps
+        const totalTyped = Math.floor((correctTyped + incorrectTyped) / gap)
+
+        // console.log('t', v.timestamps)
+        // console.log('c', v.correct)
+        // console.log('g', totalTyped)
+        // console.log('letter in 1 sec', v.correct / v.timestamps)
+        // console.log('----')
+        return {
+            timestamps: (total_time > Low) ? Math.round(v.timestamps) : v.timestamps,
+            speed: Math.floor(((v.correct - prev.correct) * WPM)),
+            correct: Math.floor((v.correct / v.timestamps) * WPM),
+            incorrect: incorrectTyped,
+            gross: totalTyped * WPM
+        }
+    })
+    const lastResult = graphData[graphData.length - 1]
+    const lastAggData = aggregatedData[aggregatedData.length - 1]
+    const lastTimestamp = gameInfo.timestamps[gameInfo.timestamps.length - 1]
+    const result = {
+        wpm: lastResult.correct,
+        graphData: graphData,
+        time: lastResult.timestamps,
+        accuracy: Math.round(Math.max(0, (1 - (lastAggData.incorrect / lastAggData.gross))) * 100),
+        remark: {
+            correct: lastTimestamp.correct,
+            error: lastTimestamp.error,
+            left: lastTimestamp.left,
+            overflow: lastTimestamp.overflow,
+        },
+        raw: Math.floor(((lastAggData.correct + lastAggData.incorrect) / lastAggData.timestamps) * WPM)
+
+    }
+    return result
+}
+
+function aggregateResut(dots: number, timestamps: Timestamps[]) {
+    console.log(timestamps)
+    const aggData: {
+        timestamps: number,
+        correct: number,
+        gross: number,
+        incorrect: number
+    }[] = []
+    // console.log(dots)
+    const state = {
+        correct: 0,
+        errors: 0,
+        left: 0,
+        overflow: 0,
+    }
+    timestamps.forEach((stamp: any, index: number) => {
+        state.correct = Math.max(state.correct, stamp.correct)
+        state.left = Math.max(state.left, stamp.left)
+        state.overflow = Math.max(state.overflow, stamp.overflow)
+        state.errors = Math.max(state.errors, stamp.error)
+        if (index + 1 == timestamps.length || (index > 0 && index % dots == 0)) {
+            // console.log('timestamps', index / 100)
+            aggData.push({
+                timestamps: index / items_in_1_sec,
+                correct: state.correct,
+                gross: state.correct + state.errors + state.overflow,
+                incorrect: state.errors + state.overflow + state.left
+            })
+            state.correct = 0
+            state.errors = 0
+            state.left = 0
+            state.overflow = 0
+        }
+    });
+
+    const lastStamp = aggData.pop()
+    if (!lastStamp) throw Error('lastStamp not found')
+    if (dots != 1 && !Number.isInteger(lastStamp?.timestamps))
+        aggData.pop()
+    aggData.push(lastStamp)
+
+    return aggData
+}
